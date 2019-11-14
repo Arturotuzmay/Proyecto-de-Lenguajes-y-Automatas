ed0b4bce15805635182e77f8a26d55820a055d19
Marco Alejandro Cupul Hau <marcoalejandro298@gmail.com>
2019-11-14 16:45:06 -0600

    Corrección de errores

--- public/contador.js
+++ public/contador.js
@@ -2,10 +2,14 @@ document.querySelector("input[type=submit]").addEventListener("click",enviar); /
  
 var resultadoEncuesta=[]; //definimos el array que contendra los respuestados de la encuesta
 var totalPreguntas=1; //definimos la variable que contiene el numero de preguntas
-var totalEncuestas=0; //variable que contiene el numero de encuestas realizadas
+var totalEncuestas=document.getElementById("total").innerHTML; //variable que contiene el numero de encuestas realizadas
 inicializarArrayResultados(); //inicializamos el array de los resultados de la forma: resultadoEncuesta["p1"]=[0,0]...
 
 function enviar(e) { //Funcion que se ejecuta cada vez que se pulsa sobre el boton "enviar"
+    totalEncuestas=document.getElementById("total").innerHTML;
+    for(var i=1;i<=totalPreguntas;i++) {
+        resultadoEncuesta["p"+i]=[document.getElementById("isc").innerHTML,document.getElementById("admin").innerHTML,document.getElementById("civil").innerHTML,document.getElementById("indus").innerHTML,document.getElementById("ambie").innerHTML];
+    }
     var preguntas=document.querySelectorAll("input[type=radio]:checked"); //obtenemos todos los radio seleccionados
     if(preguntas.length==totalPreguntas) { //si estan todos seleccionados...
         totalEncuestas++;
@@ -14,7 +18,8 @@ function enviar(e) { //Funcion que se ejecuta cada vez que se pulsa sobre el bot
             resultadoEncuesta[pregunta.name][pregunta.value]++; //guardamos en un array la respuesta
             pregunta.checked=false; //desmarcamos el check
         });
-        mostrarResultado(); //Lllama a la función mostrarResultado para que se encargue de todo lo demás
+        //mostrarResultado(); //Lllama a la función mostrarResultado para que se encargue de todo lo demás
+        enviarValores();
     }else{
         document.getElementById("error").innerHTML="Selecciona una opción";
     }
@@ -23,25 +28,17 @@ function enviar(e) { //Funcion que se ejecuta cada vez que se pulsa sobre el bot
   
 function inicializarArrayResultados() { //Funcion para inicializar el array
     for(var i=1;i<=totalPreguntas;i++) {
-        resultadoEncuesta["p"+i]=[0,0,0,0,0];
+        resultadoEncuesta["p"+i]=[document.getElementById("isc").innerHTML,document.getElementById("admin").innerHTML,document.getElementById("civil").innerHTML,document.getElementById("indus").innerHTML,document.getElementById("ambie").innerHTML];
     }
 }
-  
-function mostrarResultado() { //funcion que muestra los resultados en cada votacion
-    resultado="";
-    resultado+="<h2>De un total de "+totalEncuestas+" encuestados:</h2>";
-    for(var i=1;i<=totalPreguntas;i++) {
-        resultado+=
-        "<div>"+
-            "Ingeniería en Sistemas Computacionales: "+resultadoEncuesta["p"+i][0]+"<br>"+
-            "Ingeniería en Administración: "+resultadoEncuesta["p"+i][1]+"<br>"+
-            "Ingeniería Civil: "+resultadoEncuesta["p"+i][2]+"<br>"+
-            "Ingeniería Industrial: "+resultadoEncuesta["p"+i][3]+"<br>"+
-            "Ingeniería Ambiental: "+resultadoEncuesta["p"+i][4]+
-        "</div>";
-    }
-    //document.getElementById("resultado").innerHTML=resultado;
-    
-    addMessage(resultado); //Llama a la función que se encuentra en el main.js del public con el mensaje de resultado
-    
+
+function enviarValores(){
+    var t= totalEncuestas;
+    var v= resultadoEncuesta["p1"][0];
+    var w= resultadoEncuesta["p1"][1];
+    var x= resultadoEncuesta["p1"][2];
+    var y= resultadoEncuesta["p1"][3];
+    var z= resultadoEncuesta["p1"][4];
+    asignarValores(t,v,w,x,y,z);
+    addMessage(t);
 }
--- public/main.js
+++ public/main.js
@@ -1,4 +1,4 @@
-var socket = io.connect('http://192.168.0.6:8081', { 'forceNew': true });
+var socket = io.connect('http://192.168.61.28:8081', { 'forceNew': true });
 
 socket.on('messages', function(data) {
   console.log(data);
@@ -7,16 +7,31 @@ socket.on('messages', function(data) {
 
 function render (data) {
   var html = data.map(function(elem, index) { //Crea un arreglo que va almacenando en un arreglo
-    return(`${elem.text}`);
+    asignarValores(elem.total, elem.isc, elem.admin, elem.civil, elem.indus, elem.ambie);
+    //return(`${elem.total}`);
   });
-  document.getElementById('resultado').innerHTML = html; //Imprime el resultado en el <div> del index con el Id="resultado"
+  //document.getElementById('resultado').innerHTML = html;
 }
 
 function addMessage(e) { //Función que almacena el resultado recibido en el parámetro e y la almacena en la variable message
   var message = {
-    text: e
+    total: document.getElementById("total").innerHTML,
+    isc: document.getElementById("isc").innerHTML,
+    admin: document.getElementById("admin").innerHTML,
+    civil: document.getElementById("civil").innerHTML,
+    indus: document.getElementById("indus").innerHTML,
+    ambie: document.getElementById("ambie").innerHTML
   };
 
   socket.emit('new-message', message);
   return false;
+}
+
+function asignarValores(t,a,e,i,o,u) {
+  document.getElementById("total").innerHTML= t;
+  document.getElementById("isc").innerHTML= a;
+  document.getElementById("admin").innerHTML= e;
+  document.getElementById("civil").innerHTML= i;
+  document.getElementById("indus").innerHTML= o;
+  document.getElementById("ambie").innerHTML= u;
 }
